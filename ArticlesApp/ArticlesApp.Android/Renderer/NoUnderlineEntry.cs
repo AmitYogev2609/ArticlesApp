1f0e75e72350bf0890f7bf2087d93cb59362e44c
User <User@LAB3-15>
2021-11-15 14:19:55 +0200

    Add coustum entry

---
+++ ArticlesApp/ArticlesApp.Android/Renderer/NoUnderlineEntry.cs
@@ -0,0 +1,38 @@
+﻿using Android.App;
+using Android.Content;
+using Android.Content.Res;
+using Android.Graphics.Drawables;
+using Android.OS;
+using Android.Runtime;
+using Android.Text;
+using Android.Views;
+using Android.Widget;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+using ArticlesApp.Renderer;
+
+[assembly: ExportRenderer(typeof(ArticlesApp.MyEntry), typeof(ArticlesApp.Droid.NoUnderlineEntry))]
+namespace ArticlesApp.Droid
+{
+    public class NoUnderlineEntry : EntryRenderer
+    {
+        public NoUnderlineEntry(Context c) : base(c) { }
+        protected override void OnElementChanged(ElementChangedEventArgs<Entry> e)
+        {
+            base.OnElementChanged(e);
+
+            if (Control != null)
+            {
+                this.Control.SetBackground(null);
+                this.Control.SetPadding(35, 0, 0, 0);
+                this.Control.TextSize = 18;
+                
+            }
+        }
+
+    }
+}
---
+++ ArticlesApp/ArticlesApp/Renderer/MyEntry.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace ArticlesApp
+{
+    public class MyEntry : Entry
+    {
+        public static readonly BindableProperty FontProperty =
+            BindableProperty.Create("Font", typeof(Font),
+            typeof(MyEntry), new Font());
+
+        public Font Font
+        {
+            get { return (Font)GetValue(FontProperty); }
+            set { SetValue(FontProperty, value); }
+        }
+    }
+}
