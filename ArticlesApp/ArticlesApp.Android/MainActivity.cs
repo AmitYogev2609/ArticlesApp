203b6ad0bc4f37ef55e42eb41330ca3c945c4f5c
User <User@LAB3-15>
2021-12-15 14:40:55 +0200

    Add svg support

--- ArticlesApp/ArticlesApp.Android/MainActivity.cs
+++ ArticlesApp/ArticlesApp.Android/MainActivity.cs
@@ -13,9 +13,11 @@ namespace ArticlesApp.Droid
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
-
+           
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
             global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
+            FFImageLoading.Forms.Platform.CachedImageRenderer.Init(enableFastRenderer: true);
+            //FFImageLoading.Forms.Platform.CachedImageRenderer.InitImageViewHandler();
             LoadApplication(new App());
         }
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
--- ArticlesApp/ArticlesApp/ViewModels/SignInViewModel.cs
+++ ArticlesApp/ArticlesApp/ViewModels/SignInViewModel.cs
@@ -12,9 +12,129 @@ using System.Linq;
 
 namespace ArticlesApp.ViewModels
 {
-    public class SignInViewModel
+    public class SignInViewModel: INotifyPropertyChanged
     {
-        public Color bordercolor { get=> new Color(0, 0, 0, 0.51);  }
-        
+        #region INotifyPropertyChanged
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+        #endregion
+        public Color bordercolor { get=> new Color(0, 0, 0);  }
+        #region Email
+        private string email;
+        public string Email { get=>email; 
+            set
+            {
+                if(value!=email)
+                {
+                    email = value;
+                    OnPropertyChanged(nameof(Email));
+                }
+                if (email != "" || email != null)
+                {
+                    this.EmailBorderColor = new Color(0, 0, 0);
+                    this.showerror = false;
+                }
+            } }
+        private bool showerror;
+        public bool ShowError { get=>showerror; set
+            {
+                if(showerror!=value)
+                {
+                    showerror = value;
+                    OnPropertyChanged(nameof(ShowError));
+                }
+            } }
+        private string emailerror;
+        public string EmailError { get=>emailerror; set
+            { 
+                if(emailerror!=value)
+                {
+                    emailerror = value;
+                    OnPropertyChanged(nameof(EmailError));
+                }
+            } }
+        private Color emailbordercolor;
+        public Color EmailBorderColor
+        {
+            get => emailbordercolor;
+            set
+            {
+                if (emailbordercolor != value)
+                {
+                    emailbordercolor = value;
+                    OnPropertyChanged(nameof(EmailBorderColor));
+                }
+            }
+        }
+        #endregion
+        #region user name
+        private string username;
+        public string UserName
+        {
+            get => username;
+            set
+            {
+                if (value != username)
+                {
+                    username = value;
+                    OnPropertyChanged(nameof(UserName));
+                }
+                if (username != "" || username != null)
+                {
+                    this.UserNameBorderColor = new Color(0, 0, 0);
+                    this.ShowErrorUserName = false;
+                }
+            }
+        }
+        private bool showerrorusername;
+        public bool ShowErrorUserName
+        {
+            get => showerrorusername; set
+            {
+                if (showerrorusername != value)
+                {
+                    showerrorusername = value;
+                    OnPropertyChanged(nameof(ShowErrorUserName));
+                }
+            }
+        }
+        private string usernameerror;
+        public string UserNameError
+        {
+            get => usernameerror; set
+            {
+                if (usernameerror != value)
+                {
+                    usernameerror = value;
+                    OnPropertyChanged(nameof(UserNameError));
+                }
+            }
+        }
+        private Color usernamebordercolor;
+        public Color UserNameBorderColor
+        {
+            get => usernamebordercolor;
+            set
+            {
+                if (usernamebordercolor != value)
+                {
+                    usernamebordercolor = value;
+                    OnPropertyChanged(nameof(UserNameBorderColor));
+                }
+            }
+        }
+        #endregion
+        public SignInViewModel()
+        {
+            EmailBorderColor = new Color(0, 0, 0);
+            ShowError = true;
+            EmailError = "  ";
+            UserNameBorderColor = new Color(0, 0, 0);
+            ShowErrorUserName = true;
+            UserNameError = " ";
+        }
     }
 }
