3bc736d2839ccb4e82dc35de74546265f2cf806c
User <User@LAB3-15>
2022-04-25 14:24:27 +0300

    fdsfds

--- ArticlesApp/ArticlesApp/Views/FavoriteArticle.xaml.cs
+++ ArticlesApp/ArticlesApp/Views/FavoriteArticle.xaml.cs
@@ -17,6 +17,7 @@ namespace ArticlesApp.Views
         {
             this.BindingContext = new FavoriteArticleViewModel(); 
             InitializeComponent();
+            
         }
 
         private void colview_SelectionChanged(object sender, SelectionChangedEventArgs e)
@@ -24,7 +25,7 @@ namespace ArticlesApp.Views
             ArticleWithPicture articleWithPicture = (ArticleWithPicture)colview.SelectedItem;
             if (articleWithPicture != null)
             {
-                Page page = new ShowArticle(articleWithPicture);
+                Page page = new ShowArticle(articleWithPicture,colview);
                 colview.SelectedItem = null;
                 Navigation.PushAsync(page);
             }
--- ArticlesApp/ArticlesApp/Views/ShowArticle.xaml.cs
+++ ArticlesApp/ArticlesApp/Views/ShowArticle.xaml.cs
@@ -8,12 +8,13 @@ using ArticlesApp.ViewModels;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 using ArticlesApp.Fonts;
-
+using System.Collections.ObjectModel;
 namespace ArticlesApp.Views
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class ShowArticle : ContentPage
     {
+        public CollectionView collectionView;
         public ShowArticle(ArticleWithPicture article)
         {
             this.BindingContext= new ShowArticleViewModel(article.Article);
@@ -54,6 +55,47 @@ namespace ArticlesApp.Views
                 }
             }
         }
+        public ShowArticle(ArticleWithPicture article,CollectionView collectionView)
+        {
+            this.BindingContext = new ShowArticleViewModel(article.Article);
+            InitializeComponent();
+            ti.Text = article.Article.ArticleName;
+            HtmlWebViewSource hmlsource = new HtmlWebViewSource();
+            double width = App.Current.MainPage.Width;
+            string htmlText = article.Article.HtmlText;
+            if (!htmlText.Contains("html"))
+            {
+
+                htmlText = htmlText.Replace("width=\"auto\"", $"width =\"{width - 50}\"");
+                hmlsource.Html = "<html>\n<head>\n<meta name=\"viewport\" content=\"width=device-width, initial-scale=1\" />\n</head>\n<body>\n";
+                hmlsource.Html += htmlText;
+                hmlsource.Html += "\n</body>\n</html>";
+            }
+            else
+            {
+                //htmlText = htmlText.Replace("<html>", $"<html width =\"100%\">");
+                //htmlText = htmlText.Replace("<body>", $"<body width =\"100%\">");
+                htmlText = htmlText.Replace("<head>", "<head>\n<meta name=\"viewport\" content=\"width=device-width, initial-scale =1\">");
+                hmlsource.Html = htmlText;
+            }
+            webview.Source = hmlsource;
+
+            double height = App.Current.MainPage.Height;
+
+            grd.WidthRequest = width;
+            gtd1.WidthRequest = width;
+            webview.WidthRequest = width;
+            User logedUser = ((App)App.Current).User;
+            foreach (var obj in logedUser.FavoriteArticles)
+            {
+                if (obj.ArticleId == article.Article.ArticleId)
+                {
+                    star.Text = FontIconClass.Star;
+                    star.TextColor = Color.Gold;
+                }
+            }
+            this.collectionView = collectionView;
+        }
         protected override void OnDisappearing()
         {
             base.OnDisappearing();
@@ -75,9 +117,13 @@ namespace ArticlesApp.Views
                 star.Text = FontIconClass.StarOutline;
                 star.TextColor = Color.Black;
                 context.RemoveFavoriteArticle();
-                
+                if(collectionView!=null)
+                { 
+                    ObservableCollection<ArticleWithPicture> articles=(ObservableCollection <ArticleWithPicture>) collectionView.ItemsSource;
+                    ArticleWithPicture articleWithPicture = articles.Where(arti => arti.Article.ArticleId == context.Article.ArticleId).FirstOrDefault();
+                    articles.Remove(articleWithPicture);
+                }
             }
-
         }
     }
 }
